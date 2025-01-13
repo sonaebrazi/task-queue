90854001319209e4b088b88cd1392f6c68e011e3
Sona Ebrazi <behnam.safari@2mnordic.com>
2025-01-13 21:04:35 +0100

    project initialized

---
+++ src/test/java/com/example/task_queue_app/TaskQueueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_queue_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskQueueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
